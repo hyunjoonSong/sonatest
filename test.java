0ba4ac13e07aa5a1994827055f492a82e62603fa
hyunjoonSong <65704239+hyunjoonSong@users.noreply.github.com>
2020-05-21 16:35:31 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,18 @@
 public class test {
   
   private Integer testInt = 0;
+  
+  private boolean testboolean = true;
 
   public String execute() {
     if (testInt > 0) {
       testInt = testInt - 1;
     } else {
       testInt = 10;
+    }
+    
+    if (testboolean) {
+      log.debug("true");
     }
     
     return String.valueOf(testInt);
